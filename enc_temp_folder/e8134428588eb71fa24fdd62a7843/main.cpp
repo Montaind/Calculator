278ce10f1edb650c1f0aed5033af6526246aeff3
Богдан Грибачев <banlypro@gmail.com>
2024-03-10 15:09:02 +0300

    Edit bug: Exception

--- enc_temp_folder/e8134428588eb71fa24fdd62a7843/main.cpp
+++ enc_temp_folder/e8134428588eb71fa24fdd62a7843/main.cpp
@@ -21,7 +21,7 @@ int main()
 	double result;
 	string str;
 
-	getline(cin, str);
+	cin >> str;
 	for (int i = 0; i < str.size(); i++) {
 		if (str[i] >= '0' && str[i] <= '9') {
 			if (first_end) {
